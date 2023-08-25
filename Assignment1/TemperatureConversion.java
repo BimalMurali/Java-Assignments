3c538548d7641e38ba802262537e4395fbe24f57
Bimal Murali <71392855+BimalMurali@users.noreply.github.com>
2023-08-25 15:18:00 +0530

    Add files via upload

---
+++ Assignment1/TemperatureConversion.java
@@ -0,0 +1,13 @@
+package com.assignment.app;
+
+public class TemperatureConversion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float temp=100;
+		float tempincelsius=32-temp*(5/9);
+		System.out.println("Temperature in celsius is "+tempincelsius);
+
+	}
+
+}
