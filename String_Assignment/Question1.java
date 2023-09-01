eb10c1db5573aa3377388e075e072a3a42b8d487
Bimal Murali <71392855+BimalMurali@users.noreply.github.com>
2023-09-01 21:32:45 +0530

    Add files via upload

---
+++ String_Assignment/Question1.java
@@ -0,0 +1,21 @@
+package String_Demo;
+import java.util.Scanner;
+
+public class Question1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Enter the string : ");
+		String str=scan.nextLine();
+		int sum=0;
+		for(int i=0;i<str.length();i++){ 
+			if(str.charAt(i)>='0' && str.charAt(i)<='9'){
+				sum+=(str.charAt(i)-'0');
+			}
+		}
+		System.out.println("Sum of all digits in the string " +sum );
+
+	}
+
+}
---
+++ String_Assignment/Question2.java
@@ -0,0 +1,17 @@
+package String_Demo;
+import java.util.Scanner;
+
+public class Question2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter character : ");
+		char c = scan.next().charAt(0);
+		if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u')
+			System.out.println("Character is a vowel");
+		else
+			System.out.println("Character is a Consonant");
+	}
+
+}
