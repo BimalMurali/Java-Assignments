3b17d293030a02b1b9dc9e0023edbc6d5eb025fa
Bimal Murali <71392855+BimalMurali@users.noreply.github.com>
2023-09-01 21:56:16 +0530

    Add files via upload

---
+++ Question2/Account.java
@@ -0,0 +1,19 @@
+package Question2;
+
+public class Account {
+	static double balance=0.0;
+
+	public static double getBalance() {
+		return balance;
+	}
+
+	public static void setBalance(double balance) {
+		Account.balance = balance;
+	}
+	public void withdraw(double amount){
+		balance=balance-amount;
+	}
+	public void deposit(double amount){
+		balance=balance+amount;
+	}
+}
